91e7783f6dcc5e39512e0381cda89da1e4e5986d
Sochy CHOEUN <sochy.choeun@gmail.com>
2015-05-20 18:05:44 +0700

    Update script template with blade tags [[]]

--- src/views/javascript.blade.php
+++ src/views/javascript.blade.php
@@ -13,23 +13,25 @@
             "ordering": true,
             "order": [0, "desc"],
             "language": {
-                "paginate": {
-                    "next": "Suivant",
-                    "previous": "Précédent"
-                },
-                "info": "Montre _START_ à _END_ de _TOTAL_ entrées",
-                "infoEmpty": "Montre 0 total à 0 de 0 entrée",
-                "lengthMenu": 'Montre <select class="form-control input-sm inpu-limiter">' +
-                        '<option value="10">10</option>' +
-                        '<option value="20">20</option>' +
-                        '<option value="30">30</option>' +
-                        '<option value="40">40</option>' +
-                        '<option value="50">50</option>' +
-                        '<option value="-1">Tous</option>' +
-                        '</select> records',
-                "search": "Recherche : ",
-                "infoFiltered": "(filtrer de _MAX_ total entrées)",
-            }
+                    "paginate": {
+                        "next": "[[ l('Suivant') ]]",
+                        "previous": "[[ l('Précédent') ]]",
+                        "first": "[[ l('Premier') ]]",
+                        "last": "[[ l('Dernier') ]]"
+                    },
+                    "info": "[[ l('Montre') ]] _START_ à _END_ de _TOTAL_ [[ l('entrées') ]]",
+                    "infoEmpty": "[[ l('Montre') ]] 0 total à 0 de 0 [[ l('entrées') ]]",
+                    "lengthMenu": '[[ l("Montre") ]] <select class="form-control input-sm inpu-limiter">'+
+                        '<option value="10">10</option>'+
+                        '<option value="20">20</option>'+
+                        '<option value="30">30</option>'+
+                        '<option value="40">40</option>'+
+                        '<option value="50">50</option>'+
+                        '<option value="-1">Tous</option>'+
+                        '</select> [[ l("records") ]]',
+                    "search": "[[ l('Recherche') ]] : ",
+                    "infoFiltered": "(filtrer de _MAX_ total [[ l('entrées') ]])",
+                }
         });
     // custom values are available via $values array
     });
